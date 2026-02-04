6dd16ebe7344d9aa4fcb614c5db931c9316f75b6
Kikis06 <carola.mera20@gmail.com>
2026-02-04 08:40:25 -0600

    Add list_all function for MongoDB collection retrieval
    Implement list_all function to retrieve documents from MongoDB collection.

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+//My comment
+
+def list_all(mongo_collection):
+    """mongo_collection will be the pymongo collection object
+       Return an empty list if no document in the collection
+    """
+    documents = mongo_collection.find()
+    if documents.count() == 0:
+        return []
+    return documents
