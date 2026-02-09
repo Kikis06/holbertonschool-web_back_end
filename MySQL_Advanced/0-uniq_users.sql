37a80c68ab52e196c6eec85e645583dc75b193e8
Kikis06 <carola.mera20@gmail.com>
2026-02-09 17:41:19 -0600

    Update 0-uniq_users.sql

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,8 @@
--- Creates users table on any db
+$ cat my_script.sql
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
+$
 
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
