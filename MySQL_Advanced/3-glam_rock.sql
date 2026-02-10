6c3807e411385fd790d06ddc5584dbb9e7734fd8
Kikis06 <carola.mera20@gmail.com>
2026-02-10 10:28:29 -0600

    Update 3-glam_rock.sql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
--- This query lists all bands with Glam rock as their main style,
+-- Task 3: Old school bandlists - all bands with Glam as their main style
 -- ranked by their longevity
-SELECT band_name, IFNULL(split, YEAR(NOW())) - formed AS lifespan FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`, 2024) - `formed` as `lifespan`
+    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+    ORDER BY `lifespan` DESC;
